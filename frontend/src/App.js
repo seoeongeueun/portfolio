147fddd864392e248fd36e82661d14a8dcae0c30
seoeongeueun <erin9901@gmail.com>
2023-04-07 03:25:44 -0400

    scroll fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,66 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { useRef, useEffect } from 'react';
 import HomeContainer from './containers/HomeContainer';
 import About from './pages/About';
 import Projects from './pages/Projects';
 
 function App() {
-
   const { menu, move } = useSelector(state => ({
     menu: state.menus.menu,
     move: state.menus.move
   }));
 
+  console.log('move: ', move);
+  console.log('menu: ', menu);
+
+  const outerRef = useRef();
+  const projectRef = useRef();
+
+    useEffect(() => {
+      if (move){
+        const wheelHandler = (e) => {
+            e.preventDefault();
+            const { deltaY } = e;
+            const { scrollTop } = outerRef.current;
+            const pageHeight = window.innerHeight;
+            if (deltaY > 0) {
+                outerRef.current?.scrollTo({
+                    top: pageHeight * (Math.floor(scrollTop / pageHeight) + 1),
+                    behavior: "smooth",
+                });
+            } else {
+                outerRef.current?.scrollTo({
+                    top: pageHeight * (Math.floor(scrollTop / pageHeight) - 1),
+                    behavior: "smooth",
+                });
+            }
+        };
+        const outerRefCurr = outerRef.current;
+        outerRefCurr.addEventListener("wheel", wheelHandler);
+        return () => {
+            outerRefCurr.removeEventListener("wheel", wheelHandler);
+        };
+      }
+    }, [move]);
+
+    useEffect(() => {
+      if (move) {
+        if (menu === 'project') {
+          console.log('eyey')
+          projectRef.current?.scrollIntoView({ behavior: 'auto' });
+        }
+      }
+    }, [menu, move]);
+
   return (
       <div>
-        {(move && menu === 'about') ? <About/>
-        : (move && menu === 'project') ? <Projects/>
+        {move ? <div ref={outerRef} className='outer'>
+          <About/>
+          <Projects ref={projectRef}/>
+        </div>
         : <HomeContainer/>}
       </div>
+      
   );
 }
 
--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -5,45 +5,16 @@ import KeyboardContainer from '../containers/KeyboardContainer';
 import Projects from './Projects.js';
 
 function About() {
-    const outerRef = useRef();
-
-    useEffect(() => {
-        const wheelHandler = (e) => {
-            e.preventDefault();
-            const { deltaY } = e;
-            const { scrollTop } = outerRef.current;
-            const pageHeight = window.innerHeight;
-            if (deltaY > 0) {
-                outerRef.current?.scrollTo({
-                    top: pageHeight * (Math.floor(scrollTop / pageHeight) + 1),
-                    behavior: "smooth",
-                });
-            } else {
-                outerRef.current?.scrollTo({
-                    top: pageHeight * (Math.floor(scrollTop / pageHeight) - 1),
-                    behavior: "smooth",
-                });
-            }
-        };
-        const outerRefCurr = outerRef.current;
-        outerRefCurr.addEventListener("wheel", wheelHandler);
-        return () => {
-            outerRefCurr.removeEventListener("wheel", wheelHandler);
-        };
-    }, []);
 
     return(
         <main style={{backgroundImage: `url(${Stars})`}}>
             <div className='twinkling' style={{background: `transparent url(${Twinkling}) repeat top center`}}></div>
-            <div ref={outerRef} className='outer'>
-                <div className='aboutPage'>
-                    <div className='aboutPageIntro'>
-                        <p>About Me</p>
-                        <span>I am a front end developer who is proficient in JavaScript, HTML, CSS, and Adobe PS6.</span>
-                        <span><br/>I love coding, designing, and learning new things!</span>
-                    </div>
+            <div className='aboutPage'>
+                <div className='aboutPageIntro'>
+                    <p>About Me</p>
+                    <span>I am a front end developer who is proficient in JavaScript, HTML, CSS, and Adobe PS6.</span>
+                    <span><br/>I love coding, designing, and learning new things!</span>
                 </div>
-                <Projects/>
             </div>
         </main>
     )

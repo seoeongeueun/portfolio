8ac831e69d951046cf33a9719b618735959ce4d3
seoeongeueun <erin9901@gmail.com>
2023-07-12 04:27:32 +0900

    tip to hover mouse on image

--- frontend/src/pages/Projects2.js
+++ frontend/src/pages/Projects2.js
@@ -16,8 +16,11 @@ import Project2Sticker from '../images/project2-sticker.png';
 import Project2Edit from '../images/project2-edit.png';
 import Project2Todo from '../images/project2-todo.png';
 import Project2Search from '../images/project2-search.png';
+import Project2Add from '../images/project2-add.png';
+import Project2Friend from '../images/project2-friend.png';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import MouseIcon from '@mui/icons-material/Mouse';
 
 function Projects2({score, life, onSetScore, onSetLife, lang, setLang, onSetMenu}) {
     const [showFruit2, setShowFruit2] = useState(true);
@@ -29,7 +32,7 @@ function Projects2({score, life, onSetScore, onSetLife, lang, setLang, onSetMenu
     const [index1, setIndex1] = useState(0);
     const [found, setFound] = useState(0);
 
-    const project1 = [Project2Main, Project2Sticker, Project2Edit, Project2Right, Project2Todo, Project2Search, Project2Signup];
+    const project1 = [Project2Main, Project2Sticker, Project2Edit, Project2Right, Project2Todo, Project2Search, Project2Signup, Project2Friend, Project2Add];
 
     useEffect(() => {
         const audio = new Audio(CoinPick);
@@ -84,6 +87,8 @@ function Projects2({score, life, onSetScore, onSetLife, lang, setLang, onSetMenu
                 <div className='project'>
                     <div className='projectImageAndDetail'>
                         <div className='projectImage2'>
+                        {lang === 'English' ? <span className='tip' style={{fontSize: '0.8rem'}}><MouseIcon sx={{fontSize: '1rem', color: 'white', marginRight: '5px'}}/>Hover over image to read about each page</span> : <span className='tip'><MouseIcon sx={{fontSize: '1rem', color: 'white', marginRight: '5px'}}/>이미지에 마우스를 올려 페이지 설명 읽기</span>}
+
                             {index1 === 0 && <div className='image2'>
                                 <img src={Project2Main} alt='project2'/>
                                 {lang === 'English' ? <div className='image2Desc'>
@@ -143,7 +148,7 @@ function Projects2({score, life, onSetScore, onSetLife, lang, setLang, onSetMenu
                                         <span style={{marginBottom: '1rem'}}>이동하기 모드</span>
                                         <div className='image2DescContent'>
                                             <span>왼쪽 페이지의 오른쪽 하단 버튼 중 이동하기 버튼을 누른 후 페이지입니다. 위젯과 스티커 가운데 노란 십자가 버튼을 클릭한 뒤 드래그해서 왼쪽 페이지 내에서 원하는 위치에 위젯을 둘 수 있습니다. 이동하기 버튼을 다시 누르면 설정을 저장합니다.</span>
-                                            <span>★ 위젯과 스티커의 위치는 DB에 저장해서 새로고침하거나 다시 로그인해도 위치가 바뀌지 않게 했습니다. </span>
+                                            <span>★ 위젯과 스티커의 위치는 DB에 저장해서 새로고침하거나 다시 로그인해도 위치가 바뀌지 않게 했습니다.</span>
                                             <span>★ 기존 상태에선 드래그해도 위젯이 움직이지 않지만, Redux에 이동하기 모드를 boolean으로 저장해서 모드가 켜지면 모든 위젯 컴포넌트에서 드래그 가능하게 반응하게 했습니다. </span>
                                         </div>
                                     </div>}
@@ -176,7 +181,7 @@ function Projects2({score, life, onSetScore, onSetLife, lang, setLang, onSetMenu
                                 {lang === 'English' ? <div className='image2Desc'>
                                     <span style={{marginBottom: '1rem'}}>Todo Widget & Journal Posts Slider</span>
                                     <div className='image2DescContent'>
-                                            <span>On the left is an example of editing Todo List widget. Click on + to add more todos, check button to save, x button to remove a todo. The default face on the bottom of the Todo Widget is the frowning face, but it turns into a smiling face when finishing all the todos. 
+                                            <span>On the left is an example of editing Todo List widget. Click on + to add more todos, check button to save, x button to remove a todo. Or make the widget itself invisible by changing the settings. The default face on the bottom of the Todo Widget is the frowning face, but it turns into a smiling face when finishing all the todos. 
                                                 On the right is what the post looks like after saving it. Users can move the slider on the bottom or click on the arrow buttons to view other posts that have been written on different dates. Or directly choose a date on the Calendar widget on the left to view a post written on that date.
                                             </span>
                                             <span>★ The Todo List is unique to each day so that users can have different goals everyday. </span>
@@ -186,11 +191,102 @@ function Projects2({score, life, onSetScore, onSetLife, lang, setLang, onSetMenu
                                     : <div className='image2Desc'>
                                         <span style={{marginBottom: '1rem'}}>투두 리스트 & 일기장 슬라이더</span>
                                         <div className='image2DescContent'>
-                                            <span>왼쪽은 6가지 위젯 중 투두 리스트 설정의 예시입니다. + 버튼을 눌러 할일을 추가하고 체크 버튼을 눌러 저장하고, x 버튼을 눌러 할 일을 삭제할 수 있습니다. 투두 리스트 하단의 기본 표정은 오른쪽의 슬픈 표정이지만, 모든 할 일을 완료하면 웃는 얼굴로 바뀝니다. 
+                                            <span>왼쪽은 6가지 위젯 중 투두 리스트 설정의 예시입니다. + 버튼을 눌러 할일을 추가하고 체크 버튼을 눌러 저장하고, x 버튼을 눌러 할 일을 삭제할 수 있습니다. 또는 설정에서 아예 위젯이 보이지 않게 할 수도 있습니다. 투두 리스트 하단의 기본 표정은 오른쪽의 슬픈 표정이지만, 모든 할 일을 완료하면 웃는 얼굴로 바뀝니다.
                                                 오른쪽은 바로 전 예시에서 작성 중이었던 일기장을 저장한 뒤에 모습입니다. 하단의 슬라이더를 움직이거나 슬라이더 양쪽의 화살표 버튼을 눌러 기존에 작성한 다른 날짜의 일기로 이동할 수 있습니다. 또는 왼쪽 페이지의 달력에서 날짜를 직접 눌러서 해당 날짜의 일기로 이동할 수도 있습니다.
                                             </span>
                                             <span>★ 매일 다른 목표를 새울 수 있게 투두 리스트는 날짜마다 따로 지정할 수 있게 만들었습니다. </span>
                                             <span>★ 투두 리스트는 유저 id와 함께 날짜별로 DB에 저장하고, 유저 id가 동일한 모든 투두 리스트를 가져온 뒤, 선택된 날짜의 투두 리스트를 하나씩 보여주는 식으로 만들었습니다. </span>
+                                        </div>
+                                    </div>}
+                            </div>}
+                            {index1 === 5 && <div className='image2'>
+                                <img src={Project2Signup} alt='project2'/>
+                                {lang === 'English' ? <div className='image2Desc'>
+                                    <span style={{marginBottom: '1rem'}}>Sign Up</span>
+                                    <div className='image2DescContent'>
+                                            <span>This is what the page looks like after clicking Sign up button when it's in guest mode (not logged in). On confirm, it checks if all fields are entered, if the username is unique, if the password matches the re-entered password. If there is no problem, a message welcoming the new user appears on the bottom. 
+                                            </span>
+                                            <span>★ It looks through the DB to see if the entered username is unique.</span>
+                                            <span>★ Using Bcrpyt, password is hashed before saving in the DB.</span>
+                                        </div>
+                                    </div> 
+                                    : <div className='image2Desc'>
+                                        <span style={{marginBottom: '1rem'}}>회원가입</span>
+                                        <div className='image2DescContent'>
+                                            <span>로그인 전, 게스트 모드에서 회원가입을 눌렀을 때 페이지입니다. 모든 항목을 입력하고 CONFIRM을 누르면 중복된 닉네임 등 입력한 항목을 검사합니다. 모든 항목에서 문제가 없으면 회원가입 성공 메세지가 나타납니다.
+                                            </span>
+                                            <span>★ DB에서 유저 이름을 확인해 중복된 닉네임이 있는지 체크하고, 비밀번호 길이, 비밀번호와 재입력한 비밀번호 확인이 동일한지 검사합니다. </span>
+                                            <span>★ 비밀번호는 bcrypt를 이용해 암호화되어 DB에 저장됩니다. </span>
+                                        </div>
+                                    </div>}
+                            </div>}
+                            {index1 === 6 && <div className='image2'>
+                                <img src={Project2Add} alt='project2'/>
+                                {lang === 'English' ? <div className='image2Desc'>
+                                    <span style={{marginBottom: '1rem'}}>Notifications</span>
+                                    <div className='image2DescContent'>
+                                            <span>Users can check notifications by clicking the Notifications button shaped like a bell on the top right of the page. If someone sent the user a friend request, the user can accept or decline the request on the notifications modal. 
+                                                On accepting the request, the request sender's name is added to the receiver's friend list and vice versa. A notification that the friend request is accepted is sent to the user who sent the friend request. 
+                                                On decline, declined friend request notification is sent to the request sender. Notifications are stored up to the latest 5.
+                                            </span>
+                                            <span>★ Save the sender and receiver's id with the friend request in the DB, and switch the original sender and receiver and save the information with the receive/decline notification upon the receiver's action. </span>
+                                            <span>★ After action has been taken by the receiver, remove accept/decline buttons under the friend request notification.</span>
+                                        </div>
+                                    </div> 
+                                    : <div className='image2Desc'>
+                                        <span style={{marginBottom: '1rem'}}>알림창</span>
+                                        <div className='image2DescContent'>
+                                            <span>페이지 상단 우측 종 모양 알림 버튼을 눌러 알림을 확인할 수 있습니다. 누군가 나에게 친구 요청을 보냈다면 알림창에서 요청을 수락하거나 거절할 수 있습니다. 요청을 수락하면 친구 리스트에서 서로의 이름이 보이고, 상대방에게 요청을 수락했다는 알림을 보냅니다. 거절한다면 상대방에게 친구 요청이 거절됐다는 알림을 보냅니다. 
+                                                알림은 최근 5개까지만 보여집니다. 
+                                            </span>
+                                            <span>★ 보낸 사람의 아이디와 받는 사람의 아이디와 친구 요청 알림을 DB에 저장하고 상대방이 수락이나 거절을 하면, 반대로 기존의 보낸 사람과 받는 사람을 바꿔서 수락/거절 알림을 DB에 저장합니다. </span>
+                                            <span>★ 수락/거절 후에는 친구 신청 밑에 있던 기존 수락/거절 버튼을 보이지 않게 합니다.</span>
+                                        </div>
+                                    </div>}
+                            </div>}
+                            {index1 === 7 && <div className='image2'>
+                                <img src={Project2Friend} alt='project2'/>
+                                {lang === 'English' ? <div className='image2Desc'>
+                                    <span style={{marginBottom: '1rem'}}>Viewing Friend's Post</span>
+                                    <div className='image2DescContent'>
+                                            <span>Click on the Friend List button on the top right of the page to view the friends list. Click on the friend's name to view their posts and like each post. Contents for the posts that are set private by your friend are not visiable, but instead you see a message that the post is private. 
+                                                On the left are your widgets, but clicking on a date on the Calendar widget lets you view your friend's post written on that date. Click on the yellow bookmark with house icon on the far right to leave and go back to your own posts. 
+                                            </span>
+                                            <span>★ On liking a post, your id is added to the list of users who liked the post, and the list is updated on the DB for the liked post.</span>
+                                            <span>★ If the list of users who liked the post contains your id, liking the post removes your id from the list in the DB and undo the like. </span>
+                                        </div>
+                                    </div> 
+                                    : <div className='image2Desc'>
+                                        <span style={{marginBottom: '1rem'}}>친구의 포스팅 보기</span>
+                                        <div className='image2DescContent'>
+                                            <span>페이지 상단 우측에서 친구 버튼을 눌러 친구 목록을 확인하고 친구의 이름을 클릭해서 친구의 일기를 구경하고 좋아요를 누를 수 있습니다. 친구가 비공개한 일기는 내용이 보이지 않고 비공개라는 메세지가 뜹니다. 왼쪽 페이지는 유저의 위젯이지만, 달력에서 날짜를 선택해서 그 날짜에 작성된 친구의 일기를 볼 수 있습니다. 
+                                                우측 책갈피 중 집 모양 노란 책갈피를 눌러 다시 내 일기로 돌아올 수 있습니다.
+                                            </span>
+                                            <span>★ 좋아요를 누르면 유저의 아이디를 좋아요 리스트에 추가하고 DB에서 해당 포스팅 정보에 업데이트합니다. </span>
+                                            <span>★ 좋아요를 누른 유저 리스트에 내 아이디가 있다면, 다시 한번 버튼을 누를시엔 좋아요를 취소하고 DB에서 좋아요를 누른 유저 리스트에서 내 이름을 지웁니다. </span>
+                                        </div>
+                                    </div>}
+                            </div>}
+                            {index1 === 8 && <div className='image2'>
+                                <img src={Project2Search} alt='project2'/>
+                                {lang === 'English' ? <div className='image2Desc'>
+                                    <span style={{marginBottom: '1rem'}}>Search With Keyword</span>
+                                    <div className='image2DescContent'>
+                                            <span>Click on the Friend List button on the top right of the page to view the friends list. Click on the friend's name to view their posts and like each post. Contents for the posts that are set private by your friend are not visiable, but instead you see a message that the post is private. 
+                                                On the left are your widgets, but clicking on a date on the Calendar widget lets you view your friend's post written on that date. Click on the yellow bookmark with house icon on the far right to leave and go back to your own posts. 
+                                            </span>
+                                            <span>★ Search the DB for posts containing the given keyword and are owned by the user. </span>
+                                            <span>★ Upon clicking on the found item, change the date state of Redux store to date of the found item.</span>
+                                        </div>
+                                    </div> 
+                                    : <div className='image2Desc'>
+                                        <span style={{marginBottom: '1rem'}}>키워드 검색</span>
+                                        <div className='image2DescContent'>
+                                            <span>검색창에 원하는 키워드를 입력해서 키워드를 포함한 일기, 노트, 투두 리스트를 검색할 수 있습니다. 오른쪽 페이지의 Content나 Title을 눌러 내용으로 검색하거나, 제목으로 검색할 수 있습니다. 키워드가 포함된 모든 문장이 미리보기에 뜨고, 문장이 너무 길 경우에는 키워드를 중심으로 앞 뒤로 잘라서 보여집니다. 
+                                                검색된 일기 또는 위젯을 클릭하면 해당 날짜로 이동해서 그 날짜에 쓰여진 일기와 모든 위젯을 볼 수 있습니다. 책 모양 책갈피를 눌러서 원래 페이지로 돌아갈 수 있습니다.
+                                            </span>
+                                            <span>★ DB에서 작성자 id가 유저 id와 동일하면서 키워드를 포함한 모든 포스트를 가져옵니다.</span>
+                                            <span>★ 검색된 일기나 위젯을 누르면 Redux 스토어의 date 상태를 해당 날짜로 변경합니다. </span>
                                         </div>
                                     </div>}
                             </div>}

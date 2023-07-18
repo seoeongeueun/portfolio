ad38c7ba2574ee3abf36e01953fdfaf13e625490
seoeongeueun <erin9901@gmail.com>
2023-07-18 02:24:42 -0400

    Update Projects.js

--- frontend/src/pages/Projects.js
+++ frontend/src/pages/Projects.js
@@ -30,7 +30,7 @@ function Projects({score, life, onSetScore, onSetLife, lang, setLang}) {
     const [index1, setIndex1] = useState(0);
     const [found, setFound] = useState(0)
 
-    const project1 = [Project1Main, Project1Mypage, Project1Quizedit, Project1Takequiz, Project1Score, Project1Store, Project1Categories, Project1Leaderboard];
+    const project1 = [Project1Main, Project1Mypage, Project1Takequiz, Project1Score, Project1Store, Project1Categories, Project1Leaderboard];
 
     const handleClickFruit = (e) => {
         if (e === 'apple') {
@@ -87,7 +87,7 @@ function Projects({score, life, onSetScore, onSetLife, lang, setLang}) {
                             {lang === 'English' ? <div className='image1Desc'>
                                 <span style={{marginBottom: '0.3rem'}}>Main Page</span>
                                 <div className='image1DescContent'>
-                                        <span>This is the main page. I worked on the design and frontend of the main page. For the overall design, I aimed to keep it minimal, using blue and gray as the theme colors. Thin borders with no background color were used over full background colors to keep the design simple and clean. 
+                                        <span>This is the main page. I worked on the frontend for the main page. For the overall design, I aimed to keep it minimal, using blue and gray as the theme colors. Thin borders with no background color were used over full background colors to keep the design simple and clean. 
                                             On the top half of the page, there are draggable category cards. These cards can be dragged left and right to view more cards, and clicking on each card redirects the page to show all quizzes under the selected category. On the bottom half, there are recently created quizzes and platforms.
                                         </span>
                                         <span>★ At the bottom of each platform card, it shows the owner of the platform and the number of subscribers it currently has.</span>
@@ -97,14 +97,32 @@ function Projects({score, life, onSetScore, onSetLife, lang, setLang}) {
                                 : <div className='image1Desc'>
                                     <span style={{marginBottom: '0.3rem'}}>메인 페이지</span>
                                     <div className='image1DescContent'>
-                                        <span>로그인 후 첫 페이지입니다. 왼쪽은 위젯, 오른쪽은 일기장 페이지입니다. 왼쪽과 오른쪽 컴포넌트로 나누어 설정 및 영역은 각각 적용되고, 위젯과 스티커는 왼쪽 영역 내에서 자유롭게 배치할 수 있습니다.</span>
-                                        <span>★ ResizeObserver를 통해 화면 크기 변화를 감지하여 화면이 작아져도 위젯이 왼쪽 페이지 영역을 벗어나지 않게 만들었습니다.</span>
+                                        <span>제가 프론트엔드 작업을 한 메인 페이지입니다. 전체적인 디자인은 심플하게 만들었고, 블루와 그레이를 메인 색으로 사용했습니다. 디자인을 간결하고 깔끔하게 유지하기 만들기 위해 배경 색상 대신 배경이 없는 얇은 테두리를 많이 사용했습니다. 
+                                            페이지 상단은 드래그할 수 있는 카테고리 카드입니다. 좌우로 드래그해 다음 카드를 볼 수 있습니다. 카드를 클릭하면 그 카테고리에 속한 플랫폼을 보여주는 페이지로 이동합니다. 페이지의 하단에는 최근 만들어진 퀴즈와 플랫폼을 보여줍니다. </span>
+                                        <span>★ 각 플랫폼 카드의 하단에는 해당 플랫폼의 소유자와 현재 구독자 수가 표시됩니다.</span>
                                         <span>★ Redux를 이용해 현재 표시할 날짜를 저장하고 달력 위젯에서 원하는 날짜를 고르면 왼쪽 위젯과 오른쪽 일기가 바뀐 날짜에 맞춰 바뀝니다.</span>
-                                        <span>★ 로그인에 성공하면 서버에 이틀 후에 만료되는 쿠키를 저장하고, 클라이언트와 DB에는 유저 아이디가 담긴 토큰을 저장해서 로그인 상태를 유지하지만 쿠키가 만료되거나 유저가 로그아웃 하면 토큰을 지웁니다.</span>
+                                        <span>★ 카드에 하트 아이콘을 누르면 해당 플랫폼을 방문하지 않아도 빠르게 구독을 할 수 있습니다. </span>
                                     </div>
                             </div>}
                         </div>}
                         {index1 === 1 && <div className='image1'>
+                            <img src={Project1Categories} alt='project2'/>
+                            {lang === 'English' ? <div className='image1Desc'>
+                                <span style={{marginBottom: '0.3rem'}}>Categories</span>
+                                <div className='image1DescContent'>
+                                        <span>This is the categories page, where it displays some of the default major categories. I worked on the frontend for this page, and there was no backend work required. The hashtags beneath each category card represent the topics that users can set for their platforms within the respective category.</span>
+                                        <span>★ When users create their platform, they can choose one of these main categories and create up to 3 hashtags with any text they desire for the subtopics.</span>
+                                    </div>
+                                </div> 
+                                : <div className='image1Desc'>
+                                    <span style={{marginBottom: '0.3rem'}}>메인 페이지</span>
+                                    <div className='image1DescContent'>
+                                        <span>주요 카테고리를 보여주는 페이지입니다. 저는 프론트엔드 작업을 했고 별다른 백엔드 작업이 필요하지 않았던 페이지입니다. 카테고리 카드 하단의 해시태그는 해당 카테고리에 속한 플랫폼들이 가진 여러 서브 주제 중 3가지가 보여집니다.</span>
+                                        <span>★ 유저가 플랫폼을 만들 때 음악, 교육, 게임 등 메인 카테고리를 하나 지정하고, 최대 3가지의 서브 주제를 해시태그로 직접 작성할 수 있습니다. </span>
+                                    </div>
+                            </div>}
+                        </div>}
+                        {index1 === 2 && <div className='image1'>
                             <img src={Project1Takequiz} alt='project2'/>
                             {lang === 'English' ? <div className='image1Desc'>
                                 <span style={{marginBottom: '0.3rem'}}>Take Quiz</span>
@@ -117,16 +135,16 @@ function Projects({score, life, onSetScore, onSetLife, lang, setLang}) {
                                     </div>
                                 </div> 
                                 : <div className='image1Desc'>
-                                    <span style={{marginBottom: '0.3rem'}}>메인 페이지</span>
+                                    <span style={{marginBottom: '0.3rem'}}>퀴즈 풀기</span>
                                     <div className='image1DescContent'>
-                                        <span>로그인 후 첫 페이지입니다. 왼쪽은 위젯, 오른쪽은 일기장 페이지입니다. 왼쪽과 오른쪽 컴포넌트로 나누어 설정 및 영역은 각각 적용되고, 위젯과 스티커는 왼쪽 영역 내에서 자유롭게 배치할 수 있습니다.</span>
-                                        <span>★ ResizeObserver를 통해 화면 크기 변화를 감지하여 화면이 작아져도 위젯이 왼쪽 페이지 영역을 벗어나지 않게 만들었습니다.</span>
-                                        <span>★ Redux를 이용해 현재 표시할 날짜를 저장하고 달력 위젯에서 원하는 날짜를 고르면 왼쪽 위젯과 오른쪽 일기가 바뀐 날짜에 맞춰 바뀝니다.</span>
-                                        <span>★ 로그인에 성공하면 서버에 이틀 후에 만료되는 쿠키를 저장하고, 클라이언트와 DB에는 유저 아이디가 담긴 토큰을 저장해서 로그인 상태를 유지하지만 쿠키가 만료되거나 유저가 로그아웃 하면 토큰을 지웁니다.</span>
+                                        <span>유저가 다른 유저의 플랫폼에서 퀴즈를 푸는 화면입니다. 저는 이 페이지의 프론트엔드와 백엔드 작업을 둘 다 담당했습니다. 모든 질문은 객관식으로 이루어져 있으며, 각 질문에는 30초의 타이머가 있습니다. 시간 제한 내에 답을 고르지 않으면 자동으로 오답처리 됩니다.</span>
+                                        <span>★ 통계를 제공하고 질문의 난이도를 결정하기 위해 각 답변 선택지마다 해당 답변을 고른 사용자 수가 DB에 저장됩니다.</span>
+                                        <span>★ 타이머가 끝나기 전에 3초나 10초를 그 만큼 시간을 추가할 수 있고 일정 포인트가 차감됩니다. 힌트 버튼을 누르면 오답 중에서 한 가지 보기를 비활성화 해서 제거합니다.</span>
+                                        <span>★ 힌트와 추가 시간을 사용하면 포인트가 차감되고, 차감된 포인트는 상태로 저장되어 결과 페이지 컴포넌트에 props로 전달해서 퀴즈를 다 풀면 DB에 업데이트를 합니다.</span>
                                     </div>
                             </div>}
                         </div>}
-                        {index1 === 2 && <div className='image1'>
+                        {index1 === 3 && <div className='image1'>
                             <img src={Project1Score} alt='project2'/>
                             {lang === 'English' ? <div className='image1Desc'>
                                 <span style={{marginBottom: '0.3rem'}}>Score</span>
@@ -137,36 +155,37 @@ function Projects({score, life, onSetScore, onSetLife, lang, setLang}) {
                                     </div>
                                 </div> 
                                 : <div className='image1Desc'>
-                                    <span style={{marginBottom: '0.3rem'}}>메인 페이지</span>
+                                    <span style={{marginBottom: '0.3rem'}}>퀴즈 점수</span>
                                     <div className='image1DescContent'>
-                                        <span>로그인 후 첫 페이지입니다. 왼쪽은 위젯, 오른쪽은 일기장 페이지입니다. 왼쪽과 오른쪽 컴포넌트로 나누어 설정 및 영역은 각각 적용되고, 위젯과 스티커는 왼쪽 영역 내에서 자유롭게 배치할 수 있습니다.</span>
-                                        <span>★ ResizeObserver를 통해 화면 크기 변화를 감지하여 화면이 작아져도 위젯이 왼쪽 페이지 영역을 벗어나지 않게 만들었습니다.</span>
-                                        <span>★ Redux를 이용해 현재 표시할 날짜를 저장하고 달력 위젯에서 원하는 날짜를 고르면 왼쪽 위젯과 오른쪽 일기가 바뀐 날짜에 맞춰 바뀝니다.</span>
-                                        <span>★ 로그인에 성공하면 서버에 이틀 후에 만료되는 쿠키를 저장하고, 클라이언트와 DB에는 유저 아이디가 담긴 토큰을 저장해서 로그인 상태를 유지하지만 쿠키가 만료되거나 유저가 로그아웃 하면 토큰을 지웁니다.</span>
+                                        <span>퀴즈 점수와 결과를 보여주는 페이지입니다. 프론트엔드와 백엔드 둘 다 작업했습니다. 맞춘 질문의 갯수, 정답을 맞춰 얻은 포인트, 그리고 힌트와 타이머로 차감된 포인트가 표시됩니다. </span>
+                                        <span>★ 쉬운 질문만 반복적으로 풀면서 포인트를 파밍하는 것을 방지하기 위해, DB의 User 컬렉션에 변경 내용을 업데이트하는 API 요청은 사용자가 퀴즈를 완료한 후에 결과 페이지에서만 이루어집니다.</span>
+                                        <span>★ 퀴즈 웹사이트인 것을 감안해 각 시도는 새로운 시도로 간주되어, 유저는 이미 싫어요를 누른 퀴즈에 또 싫어요를 하거나, 이미 싫어요를 누른 퀴즈를 이번에는 좋아요를 하는 등 직전 평가에 상관없이 좋아요/싫어요를 누를 수 있습니다. 
+                                            같은 퀴즈여도 풀 때마다 유저가 새로운 지식을 얻고, 퀴즈에 대한 다른 평가를 갖게 될 수도 있기 때문입니다. </span>
                                     </div>
                             </div>}
                         </div>}
-                        {index1 === 3 && <div className='image1'>
+                        {index1 === 4 && <div className='image1'>
                             <img src={Project1Store} alt='project2'/>
                             {lang === 'English' ? <div className='image1Desc'>
                                 <span style={{marginBottom: '0.3rem'}}>My Page</span>
                                 <div className='image1DescContent'>
-                                        <span>This is store page, and I worked both frontend and backend for this page. Users can purchase icons or titles to customize their profile. A preview is available before purchase. On the header, it shows remaining points the user has and a preview of what the profile would look like with the new item. </span>
+                                        <span>This is the store page, and I worked on both the frontend and backend for this page. Users can purchase icons or titles to customize their profile. A preview is available before making a purchase. 
+                                            In the header, it shows the remaining points the user has and provides a preview of what the profile would look like with the new item. </span>
                                         <span>★ Pagination library was used to keep the number of items display at once. </span>
-                                        <span>★ Changes are made to the DB upon purchase, and the items are instantly equipped.</span>
+                                        <span>★ Changes are made to the DB upon confirming purchase by clicking Confirm on the pop-up, and the items are instantly equipped.</span>
                                     </div>
                                 </div> 
                                 : <div className='image1Desc'>
                                     <span style={{marginBottom: '0.3rem'}}>메인 페이지</span>
                                     <div className='image1DescContent'>
-                                        <span>로그인 후 첫 페이지입니다. 왼쪽은 위젯, 오른쪽은 일기장 페이지입니다. 왼쪽과 오른쪽 컴포넌트로 나누어 설정 및 영역은 각각 적용되고, 위젯과 스티커는 왼쪽 영역 내에서 자유롭게 배치할 수 있습니다.</span>
+                                        <span>프로필을 꾸밀 수 있는 아이템인 아이콘과 칭호를 구매할 수 있는 상점 페이지입니다. 프론트엔드와 백엔드 둘 다 작업했습니다. 아이템을 구매하면 포인트가 차감됩니다. Preview를 눌러 헤더에서 아이템을 장착하면 어떤 모습일지 프로필을 미리보기 할 수 있습니다.</span>
                                         <span>★ ResizeObserver를 통해 화면 크기 변화를 감지하여 화면이 작아져도 위젯이 왼쪽 페이지 영역을 벗어나지 않게 만들었습니다.</span>
-                                        <span>★ Redux를 이용해 현재 표시할 날짜를 저장하고 달력 위젯에서 원하는 날짜를 고르면 왼쪽 위젯과 오른쪽 일기가 바뀐 날짜에 맞춰 바뀝니다.</span>
-                                        <span>★ 로그인에 성공하면 서버에 이틀 후에 만료되는 쿠키를 저장하고, 클라이언트와 DB에는 유저 아이디가 담긴 토큰을 저장해서 로그인 상태를 유지하지만 쿠키가 만료되거나 유저가 로그아웃 하면 토큰을 지웁니다.</span>
+                                        <span>★ Pagination 라이브러리를 사용해서 한번에 보여지는 상점 아이템 갯수를 조절했습니다. </span>
+                                        <span>★ 팝업 창의 확정을 눌러 구매가 확정되면 DB에 변경사항을 업데이트하고, 아이템은 즉시 장착됩니다.</span>
                                     </div>
                             </div>}
                         </div>}
-                        {index1 === 4 && <div className='image1'>
+                        {index1 === 5 && <div className='image1'>
                             <img src={Project1Mypage} alt='project2'/>
                             {lang === 'English' ? <div className='image1Desc'>
                                 <span style={{marginBottom: '0.3rem'}}>My Page</span>
@@ -180,30 +199,9 @@ function Projects({score, life, onSetScore, onSetLife, lang, setLang}) {
                                 : <div className='image1Desc'>
                                     <span style={{marginBottom: '0.3rem'}}>메인 페이지</span>
                                     <div className='image1DescContent'>
-                                        <span>유저 프로필을 바꿀 수 있는 마이페이지입니다. 왼쪽과 오른쪽 컴포넌트로 나누어 설정 및 영역은 각각 적용되고, 위젯과 스티커는 왼쪽 영역 내에서 자유롭게 배치할 수 있습니다.</span>
-                                        <span>★ ResizeObserver를 통해 화면 크기 변화를 감지하여 화면이 작아져도 위젯이 왼쪽 페이지 영역을 벗어나지 않게 만들었습니다.</span>
-                                        <span>★ Redux를 이용해 현재 표시할 날짜를 저장하고 달력 위젯에서 원하는 날짜를 고르면 왼쪽 위젯과 오른쪽 일기가 바뀐 날짜에 맞춰 바뀝니다.</span>
-                                        <span>★ 로그인에 성공하면 서버에 이틀 후에 만료되는 쿠키를 저장하고, 클라이언트와 DB에는 유저 아이디가 담긴 토큰을 저장해서 로그인 상태를 유지하지만 쿠키가 만료되거나 유저가 로그아웃 하면 토큰을 지웁니다.</span>
-                                    </div>
-                            </div>}
-                        </div>}
-                        {index1 === 5 && <div className='image1'>
-                            <img src={Project1Quizedit} alt='project2'/>
-                            {lang === 'English' ? <div className='image1Desc'>
-                                <span style={{marginBottom: '0.3rem'}}>My Page</span>
-                                <div className='image1DescContent'>
-                                        <span>This is the quiz edit page for creating a quiz or editing an existing quiz. For the this page, I only worked on the design.</span>
-                                        <span>★ On the bottom of each platform card, it shows the owner of the platform and number of subscribers it has at the moment.</span>
-                                        <span>★ There are heart buttons on the platform users can click to subsribe to them without having to actually visit the platform.</span>
-                                    </div>
-                                </div> 
-                                : <div className='image1Desc'>
-                                    <span style={{marginBottom: '0.3rem'}}>메인 페이지</span>
-                                    <div className='image1DescContent'>
-                                        <span>로그인 후 첫 페이지입니다. 왼쪽은 위젯, 오른쪽은 일기장 페이지입니다. 왼쪽과 오른쪽 컴포넌트로 나누어 설정 및 영역은 각각 적용되고, 위젯과 스티커는 왼쪽 영역 내에서 자유롭게 배치할 수 있습니다.</span>
-                                        <span>★ ResizeObserver를 통해 화면 크기 변화를 감지하여 화면이 작아져도 위젯이 왼쪽 페이지 영역을 벗어나지 않게 만들었습니다.</span>
-                                        <span>★ Redux를 이용해 현재 표시할 날짜를 저장하고 달력 위젯에서 원하는 날짜를 고르면 왼쪽 위젯과 오른쪽 일기가 바뀐 날짜에 맞춰 바뀝니다.</span>
-                                        <span>★ 로그인에 성공하면 서버에 이틀 후에 만료되는 쿠키를 저장하고, 클라이언트와 DB에는 유저 아이디가 담긴 토큰을 저장해서 로그인 상태를 유지하지만 쿠키가 만료되거나 유저가 로그아웃 하면 토큰을 지웁니다.</span>
+                                        <span>유저 프로필을 바꿀 수 있는 마이페이지입니다. 저는 프론트엔드 작업을 맡았습니다. 유저는 톱니바퀴 아이콘을 클릭하여 사용자 이름과 프로필 사진을 변경할 수 있습니다. 오른쪽에는 사용자의 정보와 함께 소유한 플랫폼과 구독한 플랫폼이 표시됩니다. </span>
+                                        <span>★ Accuracy 퍼센트는 사용자가 정답을 맞힌 개수를 전체 풀이한 질문 수로 나눈 비율로 계산되며, 두 값 모두 DB에 각 사용자마다 저장됩니다.</span>
+                                        <span>★ 유저의 경험치(exp) 바는 사용자가 레벨업에 얼마나 가까운지를 보여줍니다. 유저의 현재 경험치를 다음 레벨에 필요한 경험치로 나누어 계산됩니다.</span>
                                     </div>
                             </div>}
                         </div>}
@@ -218,12 +216,11 @@ function Projects({score, life, onSetScore, onSetLife, lang, setLang}) {
                                     </div>
                                 </div> 
                                 : <div className='image1Desc'>
-                                    <span style={{marginBottom: '0.3rem'}}>메인 페이지</span>
+                                    <span style={{marginBottom: '0.3rem'}}>순위표</span>
                                     <div className='image1DescContent'>
-                                        <span>로그인 후 첫 페이지입니다. 왼쪽은 위젯, 오른쪽은 일기장 페이지입니다. 왼쪽과 오른쪽 컴포넌트로 나누어 설정 및 영역은 각각 적용되고, 위젯과 스티커는 왼쪽 영역 내에서 자유롭게 배치할 수 있습니다.</span>
-                                        <span>★ ResizeObserver를 통해 화면 크기 변화를 감지하여 화면이 작아져도 위젯이 왼쪽 페이지 영역을 벗어나지 않게 만들었습니다.</span>
-                                        <span>★ Redux를 이용해 현재 표시할 날짜를 저장하고 달력 위젯에서 원하는 날짜를 고르면 왼쪽 위젯과 오른쪽 일기가 바뀐 날짜에 맞춰 바뀝니다.</span>
-                                        <span>★ 로그인에 성공하면 서버에 이틀 후에 만료되는 쿠키를 저장하고, 클라이언트와 DB에는 유저 아이디가 담긴 토큰을 저장해서 로그인 상태를 유지하지만 쿠키가 만료되거나 유저가 로그아웃 하면 토큰을 지웁니다.</span>
+                                        <span>전체 유저 랭킹을 보여주는 순위표 페이지입니다. 저는 프론트엔드와 백엔드 작업을 둘 다 했습니다. 순위표는 기본적으로 레벨 순위로 정렬한 하나의 큰 표입니다. 레벨, 정확도, 플레이 횟수 열을 클릭해서 해당 조건대로 재정렬할 수 있습니다.  왼쪽과 오른쪽 컴포넌트로 나누어 설정 및 영역은 각각 적용되고, 위젯과 스티커는 왼쪽 영역 내에서 자유롭게 배치할 수 있습니다.</span>
+                                        <span>★ 맨 위에는 유저의 현재 순위를 보여주고, 순위표를 재정렬하면 그에 맞춰 유저 순위도 다시 계산됩니다.</span>
+                                        <span>★ 모든 유저의 아이콘, 프로필 사진, 칭호는 데이터를 가져온 순간 사용자가 장착하고 있던 아이템을 반영합니다. </span>
                                     </div>
                             </div>}
                         </div>}
